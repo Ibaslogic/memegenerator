a6e6b77fc8d88c4393df02402e55018117abb5cb
ibaslogic <ibaskunle@gmail.com>
2019-10-16 16:20:44 +0100

    Updates

---
+++ precache-manifest.03463243c6ab0fc87245a3d64cc99660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0af75215daf15aa029c5cbf9ca1a265",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "7cc7ea6ea5db3f4fb8d4",
+    "url": "/memegenerator/static/css/main.cc832123.chunk.css"
+  },
+  {
+    "revision": "524ba967d5ea09c0f64c",
+    "url": "/memegenerator/static/js/2.eeafddb4.chunk.js"
+  },
+  {
+    "revision": "7cc7ea6ea5db3f4fb8d4",
+    "url": "/memegenerator/static/js/main.ff015f1c.chunk.js"
+  },
+  {
+    "revision": "6c9a26c3decfaf1f33de",
+    "url": "/memegenerator/static/js/runtime-main.43ff85c0.js"
+  }
+]);
